5bb83db695eb995ea9e432cbb826e872eae0fcd9
Karleen Heong <17855516+karleenheong@users.noreply.github.com>
2024-09-10 21:19:02 +1000

    Add cart icon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import styles from '../styles/Navbar.module.css';
 import { NavLink } from 'react-router-dom';
+import cartIcon from '../assets/cart.png';
 
 const Navbar = ({ totalNumberItems }) => {
   return (
@@ -21,7 +22,11 @@ const Navbar = ({ totalNumberItems }) => {
             to="/cart"
             className={({ isActive }) => (isActive ? styles.hideLink : '')}
           >
-            Cart ({totalNumberItems})
+            <div className={styles.cartDiv}>
+              <img src={cartIcon} className={styles.cartIcon} /> 
+              <div className={styles.cartQuantity}>({totalNumberItems})</div>
+            </div>
+            
           </NavLink>
         </li>
       </ul>
